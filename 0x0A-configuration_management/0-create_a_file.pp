8758e359bb237e8e9384564ec751538988b2a6d5
salmenz <salmen.zooro@gmail.com>
2020-03-23 17:36:39 +0000

    Create a file using puppet code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 file { '/tmp/holberton':
   ensure  => 'file',
   path    => '/tmp/holberton',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
   content  => 'I love Puppet',
   }
