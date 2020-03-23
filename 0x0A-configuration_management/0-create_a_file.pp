7029b1a85b286ac46620b54db8c4c8826a99c826
salmenz <salmen.zooro@gmail.com>
2020-03-23 16:57:06 +0000

    Creating holberton file using puppet code

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Using Puppet, create a file in /tmp
+file { '/tmp/holberton':
+  ensure  => 'file',
+  path    => '/tmp/holberton',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content  => 'I love Puppet',
+    }
