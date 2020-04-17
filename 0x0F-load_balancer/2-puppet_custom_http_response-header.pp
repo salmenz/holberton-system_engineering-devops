5ef9e0eb64fee3245536e3b75ee94c8cc938045c
Salmen Zouari <salmen.zooro@gmail.com>
2020-04-17 21:37:19 +0200

    Update 2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,9 +1,25 @@
+#!/usr/bin/env bash
 # Install ngnix with puppet and configurate the server
 
-exec {'http_header':
-command  => 'sudo apt-get -y update && 
-             sudo apt-get -y install nginx && 
-             sudo sed -i "36i \\\tadd_header X-Served-By \$hostname;\n" /etc/nginx/sites-enabled/default &&
-             sudo service nginx restart',
-provider => shell,
+exec {'updates':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get -y update',
+}
+
+exec {'install':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get -y install nginx',
+}
+
+
+exec {'redirection':
+  provider => shell,
+  command  => 'sudo sed -i "36 i\ \tadd_header X-Served-By $(cat /etc/hostname);\n" /etc/nginx/sites-enabled/default',
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
