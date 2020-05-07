ce002b4cb2a0140af2b032b4f572c739ff7b45f7
salmenz <salmen.zooro@gmail.com>
2020-05-07 21:29:17 +0000

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+"""
+import json
+import requests
+
+
+def top_ten(subreddit):
+
+    header = {'User-Agent': 'Mozilla/5.0', 'From': 'salmen.zooro@gmail.com'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    req = requests.get(url, headers=header, allow_redirects=False)
+    hot = req.json()
+    try:
+        for i in range(10):
+            print(hot["data"]["children"][i]["data"]["title"])
+    except:
+        priint("None")
